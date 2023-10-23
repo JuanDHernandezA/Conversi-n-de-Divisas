4aec444eecadf65b260ea5e7d0008ea79da805fd
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2023-10-23 18:30:19 -0500

    Se crea el archivo divisaRouter para el manejo de peticiones

---
+++ routes/divisaRouter.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import * as divisaModel from "../models/divisa";
+import { Divisa } from '../types/divisa';
+
+const divisaRouter = express.Router();
+
+divisaRouter.get('/',async (req:Request,res:Response) => {
+    divisaModel.findAll((err:Error, divisas:Divisa[])=>{
+        if (err){
+            return res.status(500).json({ 'errorMessage': err.message }); 
+        }
+
+        res.status(200).json({'data':divisas});
+    });
+});
+
