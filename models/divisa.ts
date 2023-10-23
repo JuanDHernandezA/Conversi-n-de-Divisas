6162366f4450971aeda344e3eca1f251759bf334
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2023-10-23 18:22:45 -0500

    Se agrega los modelos de update y delete

--- models/divisa.ts
+++ models/divisa.ts
@@ -16,6 +16,46 @@ export const create = (divisa : Divisa, callback: Function)=>{
     );
 };
 
+export const findAll = (callback:Function)=>{
+    const queryString = "select * from divisa";
+
+    db.query(
+        queryString,
+        (err, result)=>{
+            if(err) {callback(err)};
+
+            const rows = <RowDataPacket[]>result;
+            const divisas : Divisa[] = [];
+
+            rows.forEach(row =>{
+                const divisa: Divisa ={
+                    id: row.id,
+                    nombre_div: row.nombre_div,
+                    iniciales_div: row.iniciales_div,
+                    vlr_referencia: row.vlr_referencia
+                };
+                divisas.push(divisa);
+            });
+            callback(divisas);
+        }
+    );
+};
+
+export const borrar = (divisa: Divisa, callback:Function)=>{
+    const queryString = "Delete from divisa where id=?";
+
+    db.query(
+        queryString,
+        divisa.id,
+        (err,result)=>{
+            if(err) {callback(err)}
+
+            const numDelete = (<ResultSetHeader>result).affectedRows;
+            callback(null,numDelete)
+        }
+    );
+};
+
 export const update = (divisa: Divisa, callback: Function)=>{
     const queryString = "update divisa set vlr_referencia = ? where id=?"
     
